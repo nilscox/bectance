3908b5f5574619b60401a19654dc07e6f15bf133
nils <nilscox.dev@gmail.com>
2025-03-20 18:59:05 +0000

    remove server-side rendering

--- src/entry-client.tsx
+++
@@ -1,7 +0,0 @@
-/* @refresh reload */
-import { hydrate } from 'solid-js/web';
-
-import { App } from './client/app';
-import './index.css';
-
-hydrate(() => <App />, document.getElementById('root'));
