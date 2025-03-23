d382f149f14714d780a375a15779cd378eaa54e0
Bopzor <26028196+Bopzor@users.noreply.github.com>
2025-03-23 21:46:21 +0100

    add tailwindcss and ark-ui

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -3,4 +3,6 @@ import { render } from 'solid-js/web';
 
 import { App } from './app';
 
+import './index.css';
+
 render(() => <App />, document.getElementById('root') as HTMLElement);
--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,12 +1,16 @@
+import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
 
 export default defineConfig({
-  plugins: [solidPlugin()],
+  plugins: [solidPlugin(), tailwindcss()],
   server: {
     port: 8000,
     proxy: {
-      '/api': { target: 'http://localhost:3000', rewrite: (path) => path.replace(/^\/api/, '') },
+      '/api': {
+        target: 'http://localhost:3000',
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
     },
   },
   build: {
