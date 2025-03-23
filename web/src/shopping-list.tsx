f51b48e7ee3fd76fe61d8eddb15894012155702b
Bopzor <26028196+Bopzor@users.noreply.github.com>
2025-03-23 23:45:22 +0100

    check item from front

--- web/src/shopping-list.tsx
+++ web/src/shopping-list.tsx
@@ -4,14 +4,18 @@ import { For, createResource, onCleanup, onMount } from 'solid-js';
 import { Checkbox } from './components/checkbox';
 
 export function ShoppingList(props: { name: string }) {
-  const list = useShoppingList(() => props.name);
+  const [list, { onItemChecked }] = useShoppingList(() => props.name);
 
   return (
     <ul>
-      <For each={list?.items}>
+      <For each={list.latest?.items}>
         {(item) => (
           <li>
-            <Checkbox label={item.product.name} checked={item.checked} />
+            <Checkbox
+              label={item.product.name}
+              checked={item.checked}
+              onChange={(checked) => onItemChecked(item.product.name, checked)}
+            />
           </li>
         )}
       </For>
@@ -22,32 +26,40 @@ export function ShoppingList(props: { name: string }) {
 function useShoppingList(getName: () => string) {
   const [list, { mutate }] = createResource(getName, getShoppingList);
 
+  const setItemChecked = (id: string, checked: boolean) => {
+    mutate((prev) => {
+      if (!prev) {
+        return prev;
+      }
+
+      const index = prev?.items.findIndex((item) => item.id === id);
+
+      if (index < 0) {
+        return prev;
+      }
+
+      return {
+        ...prev,
+        items: [
+          ...prev.items.slice(0, index),
+          { ...prev.items[index], checked },
+          ...prev.items.slice(index + 1),
+        ],
+      };
+    });
+  };
+
+  const onItemChecked = async (name: string, checked: boolean) => {
+    await checkShoppingListItem(getName(), name, checked);
+  };
+
   onMount(() => {
     const eventSource = new EventSource(`/api/shopping-list/${getName()}/events`);
 
     eventSource.addEventListener('shoppingListItemUpdated', (event) => {
       const data: { id: string; checked: boolean } = JSON.parse(event.data);
 
-      mutate((prev) => {
-        if (!prev) {
-          return prev;
-        }
-
-        const index = prev?.items.findIndex(({ id }) => id === data.id);
-
-        if (index < 0) {
-          return prev;
-        }
-
-        return {
-          ...prev,
-          items: [
-            ...prev.items.slice(0, index),
-            { ...prev.items[index], checked: data.checked },
-            ...prev.items.slice(index + 1),
-          ],
-        };
-      });
+      setItemChecked(data.id, data.checked);
     });
 
     eventSource.onerror = (err) => {
@@ -59,7 +71,7 @@ function useShoppingList(getName: () => string) {
     });
   });
 
-  return list.latest;
+  return [list, { onItemChecked }] as const;
 }
 
 async function getShoppingList(name: string) {
@@ -68,4 +80,12 @@ async function getShoppingList(name: string) {
   if (response.ok) {
     return response.json() as Promise<ShoppingList>;
   }
+}
+
+async function checkShoppingListItem(listName: string, productName: string, checked: boolean) {
+  await fetch(`/api/shopping-list/${listName}/${productName}`, {
+    method: 'PUT',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ checked }),
+  });
 }
